9bb7c6d15105927b0a56b34aad303e9f770f3135
Vinetuoj <ondrej.jebacek@gamil.com>
2023-02-26 13:44:06 +0100

    2

---
+++ python/opakování 1.části.py
@@ -0,0 +1,18 @@
+print("naučil jsem se mnoho věcí o stringu")
+print('spojeni'+' '+'stingu'+' '+'delame'+' '+'pomoci'+' '+'"+"')
+print("take jsme pouzivali print('nejaky text')")
+print("prvni radek\nDruhy radek se dela pomoci lomitka a n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
